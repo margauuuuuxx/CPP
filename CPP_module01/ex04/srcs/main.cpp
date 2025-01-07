b3d4584a3370f30a07dea70f3676e639b8a95db9
margauuuuuxx <margaux.loncour@gmail.com>
2025-01-07 13:10:02 +0100

    M01 ex04

--- CPP_module01/ex04/srcs/main.cpp
+++ CPP_module01/ex04/srcs/main.cpp
@@ -7,6 +7,12 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+
+void    error(std::string str)
+{
+    std::cout << str << std::endl;
+}
 
 std::string    fileName(std::string str)
 {
@@ -19,25 +25,38 @@ std::string    fileName(std::string str)
     return (result);
 }
 
+std::string    replace(std::string inputContent, std::string s1, std::string s2)
+{
+    std::string         result;
+    size_t              pos;
+    size_t              found;
+
+    pos = 0;
+    found = 0;
+    while ((found = inputContent.find(s1, pos)) != std::string::npos)
+    {
+        result.append(inputContent, pos, found - pos);
+        result.append(s2);
+        pos = found + s1.length();
+    }
+    result.append(inputContent, pos, std::string::npos);
+    return (result);
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 4)
-    {
-        std::cout << "Please submit a valid input:\n\t<filename> <s1> <s2>" << std::endl;
-        return (1);
-    }
-
+        return(error("Please submit a valid input:\n\t<filename> <s1> <s2>"), 1);
     std::ifstream sourceFile(argv[1]); // open the file for reading
     if (!sourceFile)
-    {
-        std::cerr << "Error opening source file" << std::endl;
-        return (1);
-    }
-
-    // std::cout << "test filename: " << fileName(argv[1]) << std::endl;
-
-    // std::string fileName = fileName(argv[1]);
-    // //std::string fileName = argv[1] + "replace";
-    // std::ofstream destFile()
-    // test << "this is a test input" << std::endl;
+        return(error("Error opening input file"), 1);
+    std::string fileContent((std::istreambuf_iterator<char>(sourceFile)),
+                                std::istreambuf_iterator<char>()); // parcourir sourcefile et le mettre dans filecontent
+    sourceFile.close();
+    std::string modifiedContent = replace(fileContent, std::string(argv[2]), std::string(argv[3]));
+    std::ofstream outputFile(fileName(argv[1])); // opening the file for writing
+    if (!outputFile)
+        return(error("Error opening ouput file"), 1);
+    outputFile << modifiedContent;
+    outputFile.close();
 }
