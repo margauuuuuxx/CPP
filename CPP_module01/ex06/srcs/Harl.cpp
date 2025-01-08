c39f8d7c4f51cad0fd9c4a94c49cdf5e3328cfb9
Margaux Loncour <marlonco@fu-r3-p5.s19.be>
2025-01-08 11:47:28 +0100

    compile linux M01 ex06

--- CPP_module01/ex06/srcs/Harl.cpp
+++ CPP_module01/ex06/srcs/Harl.cpp
@@ -3,13 +3,13 @@
 Harl::Harl() {}
 Harl::~Harl() {}
 
-void    Harl::debug() { std::cout << "[ DEBUG ]\nThis is a really nice CPP exercise that Margaux has done !\n\t\033[37m(Please read these levels messages with humor 😛)\033[0m" << std::endl; }
+void    Harl::debug( void ) { std::cout << "[ DEBUG ]\nThis is a really nice CPP exercise that Margaux has done !\n\t\033[37m(Please read these levels messages with humor 😛)\033[0m" << std::endl; }
 
-void    Harl::info() { std::cout << "[ INFO ]\nI cannot believe that this code is this short and so well written, I didn't study enough to understand it." << std::endl; }
+void    Harl::info( void ) { std::cout << "[ INFO ]\nI cannot believe that this code is this short and so well written, I didn't study enough to understand it." << std::endl; }
 
-void    Harl::warning() { std::cout << "[ WARNING ]\nI think I deserve to understand this code perfectly, so I will ask my fellow student for extensive explanations. This is almost unbearable : I must know more." << std::endl; }
+void    Harl::warning( void ) { std::cout << "[ WARNING ]\nI think I deserve to understand this code perfectly, so I will ask my fellow student for extensive explanations. This is almost unbearable : I must know more." << std::endl; }
 
-void    Harl::error() { std::cout << "[ ERROR ]\nThis is propoerly unacceptable ! I want to know more about CPP immediately !" << std::endl; }
+void    Harl::error( void ) { std::cout << "[ ERROR ]\nThis is propoerly unacceptable ! I want to know more about CPP immediately !" << std::endl; }
 
 int Harl::getLevel(std::string level)
 {
@@ -31,10 +31,13 @@ void    Harl::complain(std::string level)
     {
         case 0:
             debug();
+            // fall through
         case 1:
             info();
+            // fall through
         case 2:
             warning();
+            // fall through 
         case 3:
             error();
             break;
