a3915a3935c542dd87673e6b487eeec7c21672d3
margauuuuuxx <margaux.loncour@gmail.com>
2025-01-15 15:10:58 +0100

    M03 ex03

--- CPP_module03/ex03/srcs/DiamondTrap.cpp
+++ CPP_module03/ex03/srcs/DiamondTrap.cpp
@@ -1,22 +1,26 @@
 #include "../includes/DiamondTrap.class.hpp"
 
-DiamondTrap::DiamondTrap() : FragTrap(), ScavTrap()
+DiamondTrap::DiamondTrap() : ClapTrap(), FragTrap(), ScavTrap()
 {
     std::cout << "DiamondTrap default constructor has been called" << std::endl;
+    setName("Default");
+    ClapTrap::setName("Default_clap_name");
     this->hitPts = FragTrap::getHitsPts();
     this->energyPts = ScavTrap::getEnergyPts();
     this->attackDamage = FragTrap::getAttackDamage();
 }
 
-DiamondTrap::DiamondTrap(std::string name) : FragTrap(name), ScavTrap(name)
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap()
 {
-    std::cout << "DiamondTrap string constructir has been called" << std::endl;
+    std::cout << "DiamondTrap string constructor has been called" << std::endl;
+    ClapTrap::setName(name + "_clap_name");
+    this->name = name;
     this->hitPts = FragTrap::getHitsPts();
     this->energyPts = ScavTrap::getEnergyPts();
     this->attackDamage = FragTrap::getAttackDamage();
 }
 
-DiamondTrap::DiamondTrap(const DiamondTrap &other)
+DiamondTrap::DiamondTrap(const DiamondTrap &other) : ClapTrap(other), FragTrap(other), ScavTrap(other)
 {
     std::cout << "DiamondTrap copy constructor has been called" << std::endl;
     *this = other;
@@ -24,8 +28,22 @@ DiamondTrap::DiamondTrap(const DiamondTrap &other)
 
 DiamondTrap&    DiamondTrap::operator = (const DiamondTrap &other)
 {
+    std::cout << "DiamondTrap copy assignment operator called" << std::endl;
     if (this != &other)
     {
+        ClapTrap::operator=(other);
         setName(other.name);
+        setHitPts(other.hitPts);
+        setEnergyPts(other.energyPts);
+        setAttackDamage(other.attackDamage);
     }
+    return (*this);
+}
+
+DiamondTrap::~DiamondTrap() { std::cout << "DiamondTrap destructor called" << std::endl; }
+
+void DiamondTrap::whoAmI()
+{
+    std::cout << "Diamond trap name is : " << this->name << std::endl;
+    std::cout << "ClapTrap name is : " << ClapTrap::name << std::endl;
 }
--- CPP_module03/ex03/srcs/main.cpp
+++ CPP_module03/ex03/srcs/main.cpp
@@ -2,16 +2,46 @@
 
 int main()
 {
-    FragTrap    a("A");
-    FragTrap    b(a);
-    FragTrap    c = a;
+    std::cout << "--- Creating DiamondTrap ---" << std::endl;
+    DiamondTrap dt("Diamond");
 
-    std::cout << "Here are the stats of A:" << std::endl;
-    std::cout << "\tHit points: " << a.getHitsPts() << "\n\tEnergy points: " << a.getEnergyPts() << "\n\tAttack damage: " << a.getAttackDamage() << std::endl;
-    a.attack("the corrector correcting this");
-    b.beRepaired(10);
-    c.takeDamage(25);
-    a.highFivesGuys();
-    
-    return (0);
+    std::cout << "\n--- Testing DiamondTrap functionalities ---" << std::endl;
+
+    // Display initial stats
+    dt.whoAmI();
+
+    std::cout << "\n--- Performing actions ---" << std::endl;
+    dt.attack("enemy target"); // Inherited from ScavTrap
+    dt.takeDamage(20);         // Inherited from ClapTrap
+    dt.beRepaired(15);         // Inherited from ClapTrap
+
+    std::cout << "\n--- DiamondTrap stats after actions ---" << std::endl;
+    dt.whoAmI();
+
+    std::cout << "\n--- Additional Tests for Inheritance ---" << std::endl;
+
+    // Test FragTrap functionality
+    std::cout << "Testing FragTrap's highFivesGuys() function:" << std::endl;
+    dt.highFivesGuys();
+
+    // Test ScavTrap functionality
+    std::cout << "Testing ScavTrap's guardGate() function:" << std::endl;
+    dt.guardGate();
+
+    // Test ClapTrap attributes through DiamondTrap
+    std::cout << "\nTesting ClapTrap attributes via DiamondTrap:" << std::endl;
+    std::cout << "DiamondTrap ClapTrap::name: " << dt.ClapTrap::getName() << std::endl;
+    std::cout << "DiamondTrap hit points: " << dt.getHitsPts() << std::endl;
+    std::cout << "DiamondTrap energy points: " << dt.getEnergyPts() << std::endl;
+    std::cout << "DiamondTrap attack damage: " << dt.getAttackDamage() << std::endl;
+
+    std::cout << "\n--- Testing Copy Constructor and Assignment ---" << std::endl;
+    DiamondTrap dtCopy(dt); // Copy constructor
+    dtCopy.whoAmI();
+
+    DiamondTrap dtAssigned = dt; // Assignment operator
+    dtAssigned.whoAmI();
+
+    std::cout << "\n--- Final stats and clean up ---" << std::endl;
+    return 0;
 }
